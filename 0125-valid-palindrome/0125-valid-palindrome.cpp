f2a1567ab639b0449f3fbe4990bad600151c9398
Sai Jadhav <12jadhavsai@gmail.com>
2026-09-02 01:10:28 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0, j = s.size() - 1;
-        
-        while (i < j) {
-            while (i < j && !isalnum(s[i])) {
+
+        int i = 0;
+        int j = s.size() - 1;
+
+        while(i < j) {
+
+            if(!isalnum(s[i])) {
                 i++;
+                continue;
             }
-            while (i < j && !isalnum(s[j])) {
+
+            if(!isalnum(s[j])) {
                 j--;
+                continue;
             }
-            
-            if (tolower(s[i]) != tolower(s[j])) {
+
+            if(tolower(s[i]) != tolower(s[j])) {
                 return false;
             }
-            
+
             i++;
             j--;
         }
-        
+
         return true;
     }
 };
